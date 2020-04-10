050aba240e5430b70cc22fa7f20d245944991cc6
seregajd999 <seregajd999@gmail.com>
2020-04-10 13:35:19 +0300

    2

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const cardButton = document.querySelector('#card-button');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close');
+
+cardButton.addEventListener('click', toggleModal);
+close.addEventListener('click', toggleModal);
+
+function toggleModal(){
+  modal.classList.toggle("is-open")
+};
